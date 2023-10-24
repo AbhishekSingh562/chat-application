42314a89d9a7cc587dc7c4ad2aa8ab7df1f984a1
abhishek <abhisingh562002offc@gmail.com>
2023-10-24 22:47:23 +0530

    interface completed

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,50 @@
-const MessageForm = () => {
-  return <div>MessageForm</div>;
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { sendOutlined, PictureOutlined } from "@ant-design/icons";
+
+const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+    setValue("");
+  };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.value, text: "" });
+  };
+  return (
+    <form className="message-from" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <lable htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </lable>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+    </form>
+  );
 };
 
 export default MessageForm;
--- src/components/TheriMessage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TheriMessage() {
-  return <div>TheriMessage</div>;
-}
-
-export default TheriMessage;
